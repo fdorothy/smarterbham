02bc998b77faed8d9acfd221fe0c7ce2cd86bdd0
Fredric Dorothy <fredric.dorothy@vipaar.com>
2017-08-27 20:30:38 -0500

    added a platformio project for the arduino code

--- citysensor/arduino/src/city_sensor.ino
+++ citysensor/arduino/src/city_sensor.ino
@@ -8,6 +8,7 @@
  *  http://www.codeforbirmingham.org/
  */
 
+#include "Arduino.h"
 #include "ArduinoJson.h"
 #include "DHT.h"
 
@@ -24,16 +25,32 @@
 #define DUST_SAMPLE_TIME 280
 #define DUST_DELTA_TIME 40
 #define DUST_SLEEP_TIME 9680
-#define DUST_SAMPLES 10
 
-#define NOISE_SAMPLE_WINDOW 50
+// how many samples we take before determining
+// the noise level
+#define NOISE_SAMPLES 1024
+
+// how long between reading from the low-priority
+// sensors, in milliseconds
+#define LOW_PRIORITY_PERIOD 10000
+
+// how often we write output, in milliseconds
+#define OUTPUT_PERIOD 5000
 
 DHT dht(DHTPIN, DHTTYPE);
 float temperature = 0.0;
 float humidity = 0.0;
 float noise = 0;
-int light = 0;
+unsigned int light = 0;
 float particle = 0;
+float noiseBuffer[NOISE_SAMPLES];
+unsigned int noiseIndex;
+
+// store the last time we read from the low-priority
+// sensors, so that we only need to read from them
+// so often.
+unsigned long lastLowPriorityRead = 0;
+unsigned long lastOutput = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -46,6 +63,11 @@ void setup() {
   digitalWrite(DUST_LED_PIN, HIGH);
 }
 
+bool isAfter(unsigned long startTime, unsigned long currentTime, unsigned long period)
+{
+  return currentTime - startTime < period;
+}
+
 void readTempHumidity()
 {
   humidity = dht.readHumidity();
@@ -54,12 +76,20 @@ void readTempHumidity()
 
 void readNoise()
 {
+  if (noiseIndex >= NOISE_SAMPLES) {
+    calculateNoiseLevel();
+    noiseIndex = 0;
+  }
+  if (noiseIndex < NOISE_SAMPLES)
+    noiseBuffer[noiseIndex++] = analogRead(NOISEPIN);
+}
+
+void calculateNoiseLevel() {
   unsigned int sample;
   unsigned int sampleMax = 0;
   unsigned int sampleMin = 1024;
-  unsigned long start = millis();
-  while (millis() - start < NOISE_SAMPLE_WINDOW) {
-    sample = analogRead(NOISEPIN);
+  for (int i=0; i<NOISE_SAMPLES; i++) {
+    sample = noiseBuffer[i];
     if (sample < 1024) {
       if (sample > sampleMax)
         sampleMax = sample;
@@ -67,7 +97,7 @@ void readNoise()
         sampleMin = sample;
     }
   }
-  // output noise in volts
+  // output noise peak-to-peak in volts
   noise = (sampleMax - sampleMin) * 5.0 / 1024;
 }
 
@@ -78,21 +108,12 @@ void readLight()
 
 void readParticle()
 {
-  int vo[DUST_SAMPLES];
-  for (int i=0; i<DUST_SAMPLES; i++) {
-    digitalWrite(DUST_LED_PIN,LOW); // power on the LED
-    delayMicroseconds(DUST_SAMPLE_TIME);
-    vo[i] = analogRead(DUST_VOUT_PIN); // read the dust value
-    delayMicroseconds(DUST_DELTA_TIME);
-    digitalWrite(DUST_LED_PIN,HIGH); // turn the LED off
-    delayMicroseconds(DUST_SLEEP_TIME);
-  }
-
-  // take the average of all measurements
-  float voMeasured = 0.0;
-  for (int i=0; i<DUST_SAMPLES; i++)
-    voMeasured += vo[i];
-  voMeasured = voMeasured / DUST_SAMPLES;
+  digitalWrite(DUST_LED_PIN,LOW); // power on the LED
+  delayMicroseconds(DUST_SAMPLE_TIME);
+  float voMeasured = analogRead(DUST_VOUT_PIN); // read the dust value
+  delayMicroseconds(DUST_DELTA_TIME);
+  digitalWrite(DUST_LED_PIN,HIGH); // turn the LED off
+  delayMicroseconds(DUST_SLEEP_TIME);
   
   // 0 - 3.3V mapped to 0 - 1023 integer values
   // recover voltage
@@ -103,12 +124,26 @@ void readParticle()
   particle = 0.17 * calcVoltage - 0.1;  
 }
 
+void readLowPriority()
+{
+  unsigned long t = millis();
+  if (isAfter(lastLowPriorityRead, t, LOW_PRIORITY_PERIOD)) {
+    readTempHumidity();
+    readParticle();
+    readLight();
+    lastLowPriorityRead = t;
+  }
+}
+
+void readHighPriority()
+{
+  readNoise();
+}
+
 void readSensorData()
 {
-  readTempHumidity();
-  readLight();
-  readParticle();
-  readNoise();
+  readLowPriority();
+  readHighPriority();
 }
 
 void outputSensorData()
@@ -128,6 +163,10 @@ void outputSensorData()
 void loop() {
   // put your main code here, to run repeatedly:
   readSensorData();
-  outputSensorData();
-  delay(5000);
+
+  unsigned long t = millis();
+  if (isAfter(lastOutput, t, OUTPUT_PERIOD)) {
+    outputSensorData();
+    lastOutput = t;
+  }
 }
